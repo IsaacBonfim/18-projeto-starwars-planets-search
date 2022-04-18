8464bf3bcee2b18dbb227e0a04ec72ccfdb0591b
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-04-18 16:34:36 -0300

    Criação do arquivo MyContext.js e fetchApi.js

---
+++ src/services/fetchApi.js
@@ -0,0 +1,9 @@
+const fetchApi = async () => {
+  const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data.results;
+};
+
+export default fetchApi;

641b1af884d84dccf6d2058305160c6c97070f27
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-11 19:02:33 -0300

    Alterado o link da API que a aplicação utilizava para o link original, pois o link fornecido pela Trybe não estava funcionando e fazia a aplicação quebrar

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,5 +1,5 @@
 const fetchApi = async () => {
-  const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const endpoint = 'https://swapi.dev/api/planets/';
   const response = await fetch(endpoint);
   const data = await response.json();
 

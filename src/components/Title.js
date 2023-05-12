450acc010f999e8cf11a72c2645a620c4919252d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-12 15:06:41 -0300

    Aplicando classes css para os elementos do componente Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Title() {
   return(
     <div className='title-container'>
-      <h1>
-        <span>Star Wars</span>
-        <span>Planets Search</span>
+      <h1 className='title-h1'>
+        <span className='main-title'>Star Wars</span>
+        <span className='subtitle'>Planets Search</span>
       </h1>
     </div>
   );

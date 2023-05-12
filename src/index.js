b8864fc423f144c933da09326a4bd8b3fee72d07
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-12 14:09:01 -0300

    Agrupando a importação de todos os arquivos de Css em um mesmo no arquivo index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style/App.css';
+import './style/NameFilter.css';
+import './style/NumberFilter.css';
+import './style/NumberFiltersView.css';
+import './style/OrderColumn.css';
+import './style/Table.css';
+import './style/Title.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));

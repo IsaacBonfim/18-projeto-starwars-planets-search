a480d27d28ac3e441e48627012b74f267b7d39b7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-12 14:15:17 -0300

    Criado um componente para o Titulo da Página

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Title() {
+  return(
+    <div className='title-container'>
+      <h1>
+        <span>Star Wars</span>
+        <span>Planets Search</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Title;

3554cf2c5c54a4d69962037c231152bf047c7f04
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-14 15:30:43 -0300

    Adicionando uma div com a classe container no componente do Filtro por nomes

--- src/components/NameFilter.js
+++ src/components/NameFilter.js
@@ -4,14 +4,16 @@ import Context from '../context/Context';
 function NameFilter() {
   const { filterByName: { name }, changeName } = useContext(Context);
   return (
-    <input
-      type="text"
-      className="name-filter"
-      data-testid="name-filter"
-      value={ name }
-      onChange={ changeName }
-      placeholder="Procure um planeta"
-    />
+    <div className="container">
+      <input
+        type="text"
+        className="name-filter"
+        data-testid="name-filter"
+        value={ name }
+        onChange={ changeName }
+        placeholder="Procure um planeta"
+      />
+    </div>
   );
 }
 

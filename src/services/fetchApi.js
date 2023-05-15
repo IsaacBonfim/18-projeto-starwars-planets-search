4a5ada7ef547df8ebc44a1b4160a82f263aa5241
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-15 13:41:56 -0300

    Alterado o link da api

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,5 +1,5 @@
 const fetchApi = async () => {
-  const endpoint = 'https://swapi.dev/api/planets/';
+  const endpoint = 'https://swapi.py4e.com/api/planets/';
   const response = await fetch(endpoint);
   const data = await response.json();
 

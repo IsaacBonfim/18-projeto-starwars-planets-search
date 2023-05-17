d2662f2a20354987d9e977e153a5af7576253068
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-17 18:02:33 -0300

    Removida a importação do arquivo 'NumberFilter.css'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './style/App.css';
 import './style/NameFilter.css';
-import './style/NumberFilter.css';
 import './style/NumberFiltersView.css';
 import './style/OrderColumn.css';
 import './style/Table.css';
